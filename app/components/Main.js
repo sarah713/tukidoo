d946b37a0ccc7b9d66155f6ad0f3e8f93e3c6c80
Sarah <sarahkulsoom18@gmail.com>
2023-11-23 12:42:25 +0530

    changed subject

--- app/components/Main.js
+++ app/components/Main.js
@@ -14,7 +14,7 @@ const Main = () => {
             className=" bg-gray-200 rounded-lg border border-indigo-600"
           />
           <h2 className="text-xl font-semibold leading-relaxed text-gray-800">
-            Mathematics I
+            Algorithms 101 I
           </h2>
         </div>
         <div className="flex space-x-2">
